cea1d4a57c9b8fb85f8e985d98c7e2582227a4a9
Rob Fox <thefoxbrain@gmail.com>
2013-01-31 00:06:56 +0000

    Correct model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from mysite.books.models import Publisher, Author, Book
+from books.models import Publisher, Author, Book
 admin.site.register(Publisher)
 admin.site.register(Author)
 admin.site.register(Book)
