2165fa1beb91f96d53d4fc5da0a94f5e26476e6c
Rob Fox <thefoxbrain@gmail.com>
2013-02-01 00:04:52 +0000

    search basics 1

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 from django.conf.urls.defaults import *
 from mysite.views import hello, current_datetime, hours_ahead
+from books import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -19,4 +20,6 @@ urlpatterns = patterns('',
 	('^hello/$', hello),
 	('^time/$', current_datetime),
 	(r'^time/plus/(\d{1,2})/$', hours_ahead),
+	(r'^search-form/$', views.search_form),
+	(r'^search/$', views.search),
 )
